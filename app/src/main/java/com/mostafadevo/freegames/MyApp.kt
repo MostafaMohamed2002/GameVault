9856b21901d8a3931f2b5c57da993abfe7ed62bd
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-09-17 19:23:55 +0300

    setup timber for logging

--- app/src/main/java/com/mostafadevo/freegames/MyApp.kt
+++ app/src/main/java/com/mostafadevo/freegames/MyApp.kt
@@ -2,7 +2,12 @@ package com.mostafadevo.freegames
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApp: Application()  {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
